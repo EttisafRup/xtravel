ffb48ee230cdab7551cbf1ca5d3df10751de8bd1
Ettisaf Rup <107711625+EttisafRup@users.noreply.github.com>
2022-07-04 12:42:21 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let navul = document.querySelector('.navList');
+let nav = document.querySelector(".navList");
+let navul = document.querySelector(".navList ul");
+let navbtn = document.querySelector(".navList button");
 function hide() {
-    navul.classList.toggle('hide')
+  nav.classList.toggle("hide");
+  navul.classList.toggle("hide");
+  navbtn.classList.toggle("hide");
 }
